ace991a030d09f0042c3fe4364b405f389d39a21
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-07-25 14:56:35 -0400

    switched back to rails-assets-backbone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725174417) do
+ActiveRecord::Schema.define(version: 20140724193123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,12 +29,9 @@ ActiveRecord::Schema.define(version: 20140725174417) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "profile_id"
-    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["profile_id"], name: "index_users_on_profile_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
