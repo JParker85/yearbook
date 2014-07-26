5a9493da1c5263a4810a6049cd721ef4010753a6
MichaelByrd <michaelbyrdsemail@gmail.com>
2014-07-26 15:24:53 -0400

    forgot to load js files in rails view

---
+++ app/assets/javascripts/views/home.js
@@ -0,0 +1,12 @@
+var app = app || {};
+
+app.HomeView = Backbone.View.extend({
+  el: $('#list'),
+  events: {
+    'click': 'listUsers'
+  },
+  listUsers: function() {
+    this.$el.hide();
+    var usersView = new app.UsersView();
+  }
+});
---
+++ app/assets/javascripts/views/users.js
@@ -0,0 +1,24 @@
+var app = app || {};
+
+app.UsersView = Backbone.View.extend({
+  el: $('#users'),
+  initialize: function() {
+    this.collection = new app.Users();
+    this.collection.fetch();
+    this.render();
+    this.listenTo(this.collection, 'add', this.renderUser);
+    this.listenTo(this.collection, 'reset', this.render);
+  },
+  render: function() {
+    this.$el.append("<h1>Students List</h1>");
+    this.collection.each(function(user) {
+      this.renderUser(user);
+    }, this);
+  },
+  renderUser: function(user) {
+    var userView = new app.UserView({
+      model: user
+    });
+    this.$el.append(userView.render().el);
+  }
+});
